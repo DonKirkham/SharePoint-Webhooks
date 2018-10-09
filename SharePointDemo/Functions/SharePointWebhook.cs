aae2f379cc97e3691dc50de3d3391068bb8c3758
Don Kirkham <donkirkham@live.com>
2018-10-09 17:12:00 -0500

    Updated Settings

--- SharePointDemo/Functions/SharePointWebhook.cs
+++ SharePointDemo/Functions/SharePointWebhook.cs
@@ -18,36 +18,46 @@ namespace SharePointDemo.Functions
         [FunctionName("SharePointWebhook")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info($"Triggered started...");
-            string validationToken = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "validationtoken", true) == 0)
-                .Value;
-            if (validationToken != null)
+            try
             {
-                log.Info($"Validation token {validationToken} received");
-                HttpResponseMessage response = req.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(validationToken);
-                return response;
-            }
-
-            string content = await req.Content.ReadAsStringAsync();
-            log.Info($"Payload: {content}");
-
-            System.Collections.Generic.List<NotificationModel> notifications = JsonConvert.DeserializeObject<ResponseModel<NotificationModel>>(content).Value;
-            log.Info($"Notification count: {notifications.Count}");
-            if (notifications.Count > 0)
-            {
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("AzureWebJobsStorage"));
-                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-                CloudQueue queue = queueClient.GetQueueReference(CloudConfigurationManager.GetSetting("WebhooksQueueName"));
-                queue.CreateIfNotExists();
-                foreach (NotificationModel notification in notifications)
+                log.Info($"Triggered started...");
+                string validationToken = req.GetQueryNameValuePairs()
+                    .FirstOrDefault(q => string.Compare(q.Key, "validationtoken", true) == 0)
+                    .Value;
+                if (validationToken != null)
                 {
-                    log.Info($"Processing notification: {notification.Resource}");
-                    queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(notification)));
+                    log.Info($"Validation token {validationToken} received");
+                    HttpResponseMessage response = req.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StringContent(validationToken);
+                    return response;
                 }
+
+                string content = await req.Content.ReadAsStringAsync();
+                log.Info($"Payload: {content}");
+
+                System.Collections.Generic.List<NotificationModel> notifications = JsonConvert.DeserializeObject<ResponseModel<NotificationModel>>(content).Value;
+                log.Info($"Notification count: {notifications.Count}");
+                if (notifications.Count > 0)
+                {
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("AzureWebJobsStorage"));
+                    log.Info($"Getting connection to Storage Account: {storageAccount.ToString()}");
+                    CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                    CloudQueue queue = queueClient.GetQueueReference(CloudConfigurationManager.GetSetting("WebhooksQueueName"));
+                    log.Info($"Getting connection to Storage Queue: {queue.Name}");
+                    queue.CreateIfNotExists();
+                    foreach (NotificationModel notification in notifications)
+                    {
+                        log.Info($"Processing notification: {notification.Resource}");
+                        queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(notification)));
+                    }
+                }
+                return new HttpResponseMessage(HttpStatusCode.OK);
             }
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            catch (System.Exception ex)
+            {
+                throw ex;
+
+            }
         }
     }
 }
